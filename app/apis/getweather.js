fb79be2bdd91754c154002636ef44de3f8c3b414
Israel Cunha <israelcunhamail@gmail.com>
2020-06-22 20:56:00 -0300

    Ajuste de identação

--- app/apis/getweather.js
+++ app/apis/getweather.js
@@ -1,7 +1,8 @@
 var axios = require('axios');
 
 module.exports.weather_by_city = async function(application, city){
-    url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=eb46ab7f70b9e698ce76cebd61ca38e7&lang=pt_br'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + 
+    '&units=metric&appid=eb46ab7f70b9e698ce76cebd61ca38e7&lang=pt_br'
 
     try {
         return axios.get(url).catch(error => {
